790fa9cc41e086e029e554f9a299eaf31c0398d0
Melissa Strain <melissastrain@MelissasMacBook.lan1>
2019-06-22 17:37:16 -0400

    add full screen meta tags, try again

--- server/index.js
+++ server/index.js
@@ -11,10 +11,14 @@ const template = `
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <script src="https://use.fontawesome.com/c862f51525.js"></script>
     <script src="https://widget.cloudinary.com/global/all.js" type="text/javascript"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content = "width = device-width, initial-scale = 1.0, minimum-scale = 1, maximum-scale = 1, user-scalable = no" />
+    <meta name="apple-mobile-web-app-title" content="CDI" />
     <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="mobile-web-app-capable" content="yes">
-    <link rel="manifest" href="/manifest.json">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <link rel="apple-touch-icon" href="icon.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="icon.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="icon.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="iconpng">
     <title>CDI</title>
   </head>
   <body>
